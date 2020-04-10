c7950eeab30d151bed69093f801a3490f2d99cc7
Ighfar N S <31280935+Dontsneez@users.noreply.github.com>
2020-04-10 23:01:57 +0700

    Add files via upload

---
+++ P06-NoSQL-Database/src/produk/Koneksi.java
@@ -0,0 +1,24 @@
+package produk;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Asus
+ */
+public class Koneksi {
+
+    public static MongoDatabase sambungDB() {
+        try {
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("product");
+            System.out.println("Koneksi Sukses");
+            return database;
+        } catch (Exception e) {
+            System.out.println("Koneski gagal: "+e.getMessage());
+        }
+        return null;
+    }
+}
